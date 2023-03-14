9343f189d92daa62de37d4de2f18bd345d25e93f
seeno4ee <andrey.devel@bk.ru>
2023-03-14 12:35:13 +0300

    Генератор фрактального шума

--- FractNoise/src/main/java/org/example/Main.java
+++ FractNoise/src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
 public class Main {
+    public static final int IMAGE_WIDTH = 1280;
+    public static final int IMAGE_HEIGHT = 1024;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+            BufferedImage bufferedImage = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT, BufferedImage.TYPE_INT_RGB);
+            FractalNoise fractalNoise = new FractalNoise(512,
+                    new Random(bufferedImage.getWidth(), 100000), 9);
+            int[] pixel = ((DataBufferInt) bufferedImage.getRaster().getDataBuffer()).getData();
+            int pixelIndex = 0;
+            for (int i = 0; i < bufferedImage.getHeight(); i++) {
+                for (int j = 0; j < bufferedImage.getWidth(); j++) {
+                    int value = 0xff & (int) (fractalNoise.getValue(j, i) * 255);
+                    pixel[pixelIndex++] = 0xff000000 | value << 16 | value << 8 | value;
+                }
+            }
+
+            Utils.showImageWindow(bufferedImage, IMAGE_WIDTH, IMAGE_HEIGHT);
+        }
 }
--- FractNoise/src/main/java/org/example/Noise.java
+++ FractNoise/src/main/java/org/example/Noise.java
@@ -21,9 +21,17 @@ public class Noise {
         int yStart = yGrid * scale;
         int yEnd = yGridNext * scale;
 
-        float value12 = random.getRandomValue(xGrid,yGrid);
-        float value22 = random.getRandomValue(xGridNext,yGrid);
-        float value21 = random.getRandomValue(xGridNext,yGridNext);
-        float value11 = random.getRandomValue(xGrid,yGridNext);
+        float value12 = random.getRandomValue(xGrid, yGrid);
+        float value22 = random.getRandomValue(xGridNext, yGrid);
+        float value21 = random.getRandomValue(xGridNext, yGridNext);
+        float value11 = random.getRandomValue(xGrid, yGridNext);
+
+        float w12 = ((float) (xEnd - x) * (yEnd - y)) / ((xEnd - xStart) * (yEnd - yStart));
+        float w22 = ((float) (x - xStart) * (yEnd - y)) / ((xEnd - xStart) * (yEnd - yStart));
+        float w21 = ((float) (x - xStart) * (y - yStart)) / ((xEnd - xStart) * (yEnd - yStart));
+        float w11 = ((float) (xEnd - x) * (y - yStart)) / ((xEnd - xStart) * (yEnd - yStart));
+
+        return value11 * w11 + value12 * w12 + value21 * w21 + value22 * w22;
+
     }
 }
